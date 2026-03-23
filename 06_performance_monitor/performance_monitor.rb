11ea0f5ba5c6a5623f82672fad09338e593fc9e3
RobertOltean314 <olteanrobertadrian314@gmail.com>
2026-03-23 17:33:17 +0200

    Implement feedback

--- 06_performance_monitor/performance_monitor.rb
+++ 06_performance_monitor/performance_monitor.rb
@@ -1,7 +1,6 @@
-def measure(num = 1, &block)
-  # &block -> transforms the block into an object so we can pass it like one
+def measure(num = 1)
   start = Time.now
-  num.times { block.call } # we could do this because we took the block as an object
+  num.times { yield }
   passed = Time.now - start
   passed.to_f / num
 end
