c737ded3d5e3717b98cd0e9663b89cf801a5b214
RobertOltean314 <olteanrobertadrian314@gmail.com>
2026-03-23 16:39:47 +0200

    Implement feedback

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,28 +1,23 @@
+VOWELS = %w{a e i o u}
+
 def translate(phrase)
-  # apple - > starts with vowel -> apple + ay = appleay
-  # banana -> starts with consonant -> banana -> move 'b' at the end = ananab -> + ay = ananabay
-  # qu -> is considered an edge case
-  vowels = %w{a e i o u}
-
-  # for each char in word
-  # while word[0] != vowels
-  # word[:-1] = word[0]
-  # end while
-  # word + ay
-
   phrase.split(" ").map do |word|
-    is_capitalized = word[0] == word[0].upcase # if the words starts with a capitalized letter, keep it, otherwise it should start with a lower letter
-
-    word = word.downcase
-
-    until vowels.include?(word[0]) # if word[0] is not in vowel it's a consonant
-      if word[0] == "q" && vowels.include?(word[1])
-        word = word[2..] + "qu" # add both letters at the end
-      else
-        word = word[1..] + word[0]
-      end
-    end
-    result = word + "ay"
-    is_capitalized ? result.capitalize : result
-  end.join(" ") # looping for each word and apply the algorithm for each word
+    translate_word(word)
+  end.join(" ")
 end
+
+def translate_word(word)
+  is_capitalized = word[0] == word[0].upcase
+  word = word.downcase
+
+  until VOWELS.include?(word[0])
+    if word[0] == "q" && VOWELS.include?(word[1])
+      word = word[2..] + "qu"
+    else
+      word = word[1..] + word[0]
+    end
+  end
+  result = word + "ay"
+  is_capitalized ? result.capitalize : result
+end
+
