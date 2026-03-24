296fd52bb91473be6f219a6ddde8575f2ad2fbc4
RobertOltean314 <olteanrobertadrian314@gmail.com>
2026-03-24 10:29:21 +0200

    Solve exercise 09

--- 09_timer/timer.rb
+++ 09_timer/timer.rb
@@ -1,17 +1,27 @@
 class Timer
-  attr_accessor :seconds
-  # if store something -> class field
-  # if not store something -> method
+  # 1. Constants
+  # 2. Class-level macros (attr_*, include, extend, etc.)
+  # 3. Class Methods
+  # 4. Public instance methods
+  # 5. Protected Methods
+  # 6. Private Methods
+  attr_reader :seconds
 
-  def time_string # time_string doesn't store anything -> method
+  def initialize(seconds = 0)
+    self.seconds = seconds
+  end
+
+  def time_string
     hours = seconds / 3600
     minutes = (seconds % 3600) / 60
     secs = seconds % 60
     format("%02d:%02d:%02d", hours, minutes, secs)
   end
 
-  def initialize(seconds = 0)
-    # seconds stores a value -> class field
-    self.seconds = seconds
+  def padded(seconds)
+    padded = seconds.to_s.rjust(2, '0')
+    padded
   end
+
+  private attr_writer :seconds
 end
