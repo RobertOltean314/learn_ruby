6783f206057bdde6fa1f7b67eed5ee980f5047ba
RobertOltean314 <olteanrobertadrian314@gmail.com>
2026-03-27 13:18:08 +0200

    Implement feedback

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -1,35 +1,27 @@
-class Book
-  # attr_reader :name -> this acts as a getter
-  # attr_accessor :name -> this acts as a getter + setter
-  # attr_writer :name -> this acts as a setter
-  def title
-    @title
-  end
+# frozen_string_literal: true
 
-  # def title(title)
-  #   @title = title.split.map(&:capitalize).join(' ')
-  # end
+module TextHelpers
+  LITTLE_WORDS = %w[and or a an of in on at to from over the].freeze
+end
+
+class Book # rubocop:disable Style/Documentation
+  attr_reader :title
 
   def title=(title)
-    # title=(title) -> setter method
-    # copied from ex 3 -> same logic goes for capitalizing titles
-    little_words = %w[and or a an of in on at to from over the] # %w[] -> array of strings
     words = title.downcase.split
 
     capitalized = words.map.with_index do |word, index|
-      if little_words.include?(word) && index != 0
+      if TextHelpers::LITTLE_WORDS.include?(word) && index != 0
         word
       else
         word.capitalize
       end
     end
 
-    @title = capitalized.join(" ")
-
+    @title = capitalized.join(' ')
   end
 
-  def initialize(title = "")
-    # this is called using Book.new
+  def initialize(title = '')
     self.title = title
   end
 end
